8ed3b03509ed73ac42b6a37dd83e5419df6ca20d
Wilfred Ruck <wilfredoruck@gmail.com>
2018-03-02 13:11:17 -0500

    created model for contact and view pages

---
+++ Ouzi/ouzi/app/models/contact.rb
@@ -0,0 +1,12 @@
+class Contact < MailForm::Base
+	attribute :name, :validate => true
+	attribute :email, :validate => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	attribute :message, :validate => true
+	attribute :nickname, :captcha => true
+
+	def headers {
+		:subject => "Contact Form",
+		:to => "wilfredoruck@gmail.com",
+		:from => %("#{name}" <#{email}>)
+	}
+end
